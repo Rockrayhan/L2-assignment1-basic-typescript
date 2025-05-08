fa09ad13011364ebe2efa514db9ffbac12dc2dba
Rockrayhan <khayrulalamdict@gmail.com>
2025-05-08 12:38:51 +0600

    all -8 problems solved

--- index.ts
+++ index.ts
@@ -20,15 +20,11 @@
 
 
 
-
-
   // 2. Create a function that filters an array of objects by the rating property, returning only items with a rating of 4 or more.
 
 
   // function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
-  //   const myItem = (items.map(item => item.rating ));
-    
-  //   return 
+  //   return items.filter( item => item.rating >= 4 )
     
   // }
 
@@ -38,14 +34,79 @@
   //   { title: "Book C", rating: 5.0 }
   // ];
   
-  // filterByRating(books);
-
+  // console.log(filterByRating(books));
 
 
 // ====================================================
 
 
 
+
+// 3. Create a generic function that concatenates multiple arrays of the same type using rest parameters.
+
+// function concatenateArrays<T>(...arrays: T[][]): T[] {
+
+//   let result :T[] = [] ;
+
+//   for( let i = 0; i < arrays.length; i++ ) {
+//     result = result.concat(arrays[i])
+//   }
+//   return result ;
+
+// }
+
+// console.log(concatenateArrays(["a", "b"], ["c"]));
+// console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+
+
+
+// =============================================================
+
+
+
+// 4. Create a Vehicle class with private make and year properties and a getInfo() method. Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+
+
+
+
+// class Vehicle {
+//   private make: string;
+//   private year: number;
+
+//   constructor(make: string, year: number) {
+//     this.make = make;
+//     this.year = year;
+//   }
+
+//   getInfo() {
+//     console.log(`Make: ${this.make}, Year: ${this.year}`);
+//   }
+// }
+
+// class Car extends Vehicle {
+//   private model: string;
+
+//   constructor(make: string, year: number, model: string) {
+//     super(make, year);
+//     this.model = model;
+//   }
+
+//   getModel() {
+//     console.log("Model: " + this.model);
+//   }
+// }
+
+// const myCar = new Car("Toyota", 2022, "k-99");
+
+// myCar.getModel();  
+// myCar.getInfo();
+
+
+
+
+
+// =========================================================================
 
 
 
@@ -60,7 +121,7 @@
 //     else return value.length ;
 // }
 
-// console.log(processValue(""));
+// console.log(processValue(10));
 
 
 // ============================================================
@@ -71,6 +132,8 @@
 
 
 // 6. Define an interface Product and create a function to find the product with the highest price in an array. If the array is empty, return null.
+
+
 
 // interface Product {
 //   name: string;
@@ -112,9 +175,6 @@
 
 
 
-
-
-
   // 7. Create a function that returns "Weekday" or "Weekend" based on the input day.
 
 //   enum Day {
@@ -149,27 +209,27 @@
 // 8. Create an async function that: Returns the square of a number after 1 second , Rejects if the number is negative
 
 
-function waitOneSecond() {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-}
+// function waitOneSecond() {
+//   return new Promise<void>((resolve) => {
+//     setTimeout(() => {
+//       resolve();
+//     }, 1000);
+//   });
+// }
 
-async function squareAsync(n: number): Promise<number> {
-  if (n < 0) {
-    throw new Error("Number cannot be negative");
-  }
+// async function squareAsync(n: number): Promise<number> {
+//   if (n < 0) {
+//     throw new Error("Number cannot be negative");
+//   }
 
-  await waitOneSecond(); 
-  return n * n;
-}
+//   await waitOneSecond(); 
+//   return n * n;
+// }
 
 
-squareAsync(6)
-  .then(console.log)        
-  .catch(console.error);
+// squareAsync(6)
+//   .then(console.log)        
+//   .catch(console.error);
 
 // squareAsync(-3)
 //   .then(console.log)
